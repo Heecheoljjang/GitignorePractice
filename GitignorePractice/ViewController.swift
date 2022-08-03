dc4e3fae4a6165c109cb21dab146dc87d84fafef
Heecheoljjang <kkll135@naver.com>
2022-08-04 00:23:14 +0900

    Initial Commit

---
+++ GitignorePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitignorePractice
+//
+//  Created by HeecheolYoon on 2022/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
